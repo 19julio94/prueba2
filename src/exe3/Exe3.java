2529406430e0bb7f03d653becaae302de6caf1bc
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-10-05 09:04:39 +0200

    inicio

---
+++ src/exe3/Exe3.java
@@ -0,0 +1,15 @@
+
+package exe3;
+
+
+public class Exe3 {
+    static int num2;
+
+public static void main(String[] args) {
+   
+    int valor = 3,resultado; 
+resultado = valor ++;
+System.out.println("valor = "+valor+" resultado =" +resultado);
+    }
+    
+}
